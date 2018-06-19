2eba1c0ca5f8b297b59718babe3a8988eb293d97
ChrstnDmbchr <christiandambacher@gmail.com>
2018-06-19 09:57:59 +0100

    refactored and modified users controller based on review

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,6 +3,9 @@ const model = require('../models');
 exports.getUser = (req, res, next) => {
   model.User.findOne({username: req.params.username})
   .then(user => {
+    if (!user) {
+      return next({status: 404, error: 'user not found'});
+    };
     res.status(200).send(user);
   })
   .catch(err => next({status: 500, error: err}));
