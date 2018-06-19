cf75db41c546c0b1143ccf81e9a722b30803b2df
ChrstnDmbchr <christiandambacher@gmail.com>
2018-06-19 09:51:37 +0100

    refactored and added additional error handling to comment controller based on review

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -1,17 +1,23 @@
 const models = require('../models');
 
 exports.commentVote = (req, res, next) => {
-  if (!Object.keys(req.query).includes('vote')) {
-    return res.status(400).send({
-      error: "invalid query, please user the 'vote' query followed by up/down"
-    });
+  if (!req.query.hasOwnProperty('vote')) {
+    return next({status: 400, error: "invalid query, please user the 'vote' query followed by up/down"});
   };
-  let vote
-  if (req.query.vote === 'up') vote = 1
-  else if (req.query.vote === 'down') vote = -1
 
-  models.Comment.findByIdAndUpdate({_id: req.params.comments_id}, {$inc: {votes: vote}}, { new: true })
+  if(req.query.vote !== 'up' && req.query.vote !== 'down') {
+    return next({status: 400, error: "invalid query, please user the 'vote' query followed by up/down"});
+  }
+
+  let vote;
+  if (req.query.vote === 'up') vote = 1;
+  else if (req.query.vote === 'down') vote = -1;
+
+  models.Comment.findByIdAndUpdate(req.params.comments_id, {$inc: {votes: vote}}, { new: true })
   .then(comment => {
+    if (!comment) {
+      return next({status: 404, error: 'Comment not found'});
+    };
     res.status(201).send({
       message: `article vote modified by ${vote}!`,
       vote_count: comment.votes
@@ -21,12 +27,15 @@ exports.commentVote = (req, res, next) => {
 };
 
 exports.deleteComment = (req, res, next) => {
-  models.Comment.findByIdAndRemove({_id: req.params.comments_id})
+  models.Comment.findByIdAndRemove(req.params.comments_id)
   .then(comment => {
+    if (!comment) {
+      return next({status: 404, error: 'Comment not found'});
+    };
     res.status(200).send({
       message: "comment deleted",
       comment: comment
-    })
+    });
   })
   .catch(err => next({status: 500, error: err}));
 };
